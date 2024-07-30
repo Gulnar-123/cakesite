a306762ddfb4c305340c120e441cfecc6f362bc1
Gulnar-123 <gulnar6816@gmail.com>
2024-07-30 23:27:16 +0530

    Prj

---
+++ cakesite/src/main/java/com/bakery/cakesite/CakesiteApplication.java
@@ -0,0 +1,13 @@
+package com.bakery.cakesite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CakesiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CakesiteApplication.class, args);
+	}
+
+}
---
+++ cakesite/src/test/java/com/bakery/cakesite/CakesiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bakery.cakesite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CakesiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
